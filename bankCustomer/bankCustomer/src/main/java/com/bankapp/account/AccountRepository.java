f1c74d4875723e70775f794a2751293a35a4ec27
JirPer <114312839+JirPer@users.noreply.github.com>
2023-05-01 20:23:10 +0200

    Add files via upload

---
+++ bankCustomer/bankCustomer/src/main/java/com/bankapp/account/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.bankapp.account;
+
+import com.bankapp.customer.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+
+        Optional<Account> findByAccountName(String accountName);
+}
