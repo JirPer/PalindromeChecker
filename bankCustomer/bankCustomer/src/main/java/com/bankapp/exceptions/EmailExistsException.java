170e9b784f26e2404a0fab4b516f5c4cca92794e
JirPer <114312839+JirPer@users.noreply.github.com>
2023-05-14 16:33:10 +0200

    Delete bankCustomer/bankCustomer directory

--- bankCustomer/bankCustomer/src/main/java/com/bankapp/exceptions/EmailExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.bankapp.exceptions;
-
-public class EmailExistsException extends RuntimeException {
-    public EmailExistsException(String message) {
-        super(message);
-    }
-}
--- bankCustomer/bankCustomer/src/main/java/com/bankapp/transactions/TransactionsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bankapp.transactions;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionsRepository extends JpaRepository<Transactions, Long> {
-
-}
